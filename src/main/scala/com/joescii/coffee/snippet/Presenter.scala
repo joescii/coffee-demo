5bb1bb3692a1041eb004da4e06af7f6e970c89e2
Joe Barnes <barnesjd@gmail.com>
2014-11-04 09:20:11 -0600

    Added client slide controls

---
+++ src/main/scala/com/joescii/coffee/snippet/Presenter.scala
@@ -0,0 +1,21 @@
+package com.joescii.coffee.snippet
+
+import net.liftweb.http._
+import js._
+import JsCmds._
+import JE._
+import net.liftweb.util.Helpers._
+import net.liftweb.util.ClearNodes
+import com.joescii.coffee.comet.Presentation
+
+/**
+ * Created by jbarnes on 10/27/2014.
+ */
+object Presenter {
+  def render = if(Host.isLocal) "*" #> Script(
+    JsCrVar("PresenterApi", JsObj()) &
+    SetExp(JsVar("PresenterApi.goto"), AnonFunc("id", SHtml.ajaxCall(JsVar("id"), {(id:String) =>
+      Presentation ! id
+    })))
+  ) else ClearNodes
+}
